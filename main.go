0d8237b500342ff9fa40f745bc35cd61b0d7bfc9
nparsons <parsons.nicholas1@gmail.com>
2020-09-28 12:13:02 -0400

    Added test code to count lines.

---
+++ main.go
@@ -0,0 +1,18 @@
+// count tallies the number of times each line
+// occurs within a file.
+package main
+
+import (
+  "fmt"
+  "github.com/nparsons/datafile"
+  "log"
+)
+
+func main() {
+  lines, err := datafile.GetStrings("votes.txt")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(lines)
+}
